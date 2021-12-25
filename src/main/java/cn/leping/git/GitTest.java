d41e9bb07048d9780020db219ab2f2b51d5eec0f
lepingyang <lepingyang@trip.com>
2021-12-25 22:54:24 +0800

    first commit

---
+++ src/main/java/cn/leping/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.leping.git;
+
+/**
+ * @package:cn.leping.git
+ * @user LePingYang
+ * @date 2021/12/25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}

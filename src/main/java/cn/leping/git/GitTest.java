4e34ad3b5ca152776ff304bc11f5be9312ed8eed
2190829778 <75290736+2190829778@users.noreply.github.com>
2021-12-26 23:26:59 +0800

    github update

--- src/main/java/cn/leping/git/GitTest.java
+++ src/main/java/cn/leping/git/GitTest.java
@@ -8,5 +8,6 @@ package cn.leping.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("github test!");
     }
 }
